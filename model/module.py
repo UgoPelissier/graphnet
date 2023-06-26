c75a96d8e4dd3043927e3f7096d93cce72f82e78
UgoPelissier <upelissier@gmail.com>
2023-06-26 07:51:50 +0000

    ♻️ Refactor stats loading

--- model/module.py
+++ model/module.py
@@ -3,7 +3,7 @@ from typing import Optional, List, Tuple, Union
 import os.path as osp
 import json
 
-from utils.utils import get_next_version, make_animation
+from utils.utils import load_stats, get_next_version, make_animation
 from model.processor import ProcessorLayer
 
 import torch
@@ -177,29 +177,10 @@ class MeshGraphNet(pl.LightningModule):
         
     def load_stats(self):
         """Load statistics from the dataset."""
-        train_dir = osp.join(self.dataset, 'processed', 'stats', 'train')
-        self.mean_vec_x_train = torch.load(osp.join(train_dir, 'mean_vec_x.pt'), map_location=self.device)
-        self.std_vec_x_train = torch.load(osp.join(train_dir, 'std_vec_x.pt'), map_location=self.device)
-        self.mean_vec_edge_train = torch.load(osp.join(train_dir, 'mean_vec_edge.pt'), map_location=self.device)
-        self.std_vec_edge_train = torch.load(osp.join(train_dir, 'std_vec_edge.pt'), map_location=self.device)
-        self.mean_vec_y_train = torch.load(osp.join(train_dir, 'mean_vec_y.pt'), map_location=self.device)
-        self.std_vec_y_train = torch.load(osp.join(train_dir, 'std_vec_y.pt'), map_location=self.device)
-
-        val_dir = osp.join(self.dataset, 'processed', 'stats', 'valid')
-        self.mean_vec_x_val = torch.load(osp.join(val_dir, 'mean_vec_x.pt'), map_location=self.device)
-        self.std_vec_x_val = torch.load(osp.join(val_dir, 'std_vec_x.pt'), map_location=self.device)
-        self.mean_vec_edge_val = torch.load(osp.join(val_dir, 'mean_vec_edge.pt'), map_location=self.device)
-        self.std_vec_edge_val = torch.load(osp.join(val_dir, 'std_vec_edge.pt'), map_location=self.device)
-        self.mean_vec_y_val = torch.load(osp.join(val_dir, 'mean_vec_y.pt'), map_location=self.device)
-        self.std_vec_y_val = torch.load(osp.join(val_dir, 'std_vec_y.pt'), map_location=self.device)
-
-        test_dir = osp.join(self.dataset, 'processed', 'stats', 'test')
-        self.mean_vec_x_test = torch.load(osp.join(test_dir, 'mean_vec_x.pt'), map_location=self.device)
-        self.std_vec_x_test = torch.load(osp.join(test_dir, 'std_vec_x.pt'), map_location=self.device)
-        self.mean_vec_edge_test = torch.load(osp.join(test_dir, 'mean_vec_edge.pt'), map_location=self.device)
-        self.std_vec_edge_test = torch.load(osp.join(test_dir, 'std_vec_edge.pt'), map_location=self.device)
-        self.mean_vec_y_test = torch.load(osp.join(test_dir, 'mean_vec_y.pt'), map_location=self.device)
-        self.std_vec_y_test = torch.load(osp.join(test_dir, 'std_vec_y.pt'), map_location=self.device)
+        train_stats, val_stats, test_stats = load_stats(self.dataset, self.device)
+        self.mean_vec_x_train, self.std_vec_x_train, self.mean_vec_edge_train, self.std_vec_edge_train, self.mean_vec_y_train, self.std_vec_y_train = train_stats
+        self.mean_vec_x_val, self.std_vec_x_val, self.mean_vec_edge_val, self.std_vec_edge_val, self.mean_vec_y_val, self.std_vec_y_val = val_stats
+        self.mean_vec_x_test, self.std_vec_x_test, self.mean_vec_edge_test, self.std_vec_edge_test, self.mean_vec_y_test, self.std_vec_y_test = test_stats
         
     def normalize(self, x: torch.Tensor, edge_attr: torch.Tensor, labels: torch.Tensor, split) -> torch.Tensor:
         if split == 'train':
--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import os.path as osp
 import logging
 from typing import List, Tuple, Union, Optional, Dict, Any
 
@@ -8,8 +9,46 @@ from matplotlib import tri as mtri
 from matplotlib import animation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
+import torch
 from torch_geometric.data import Data
 from lightning.fabric.utilities.cloud_io import get_filesystem
+
+
+def load_stats(
+        dataset: str,
+        device: str
+        ) -> Tuple[Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+                   Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+                   Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]]:
+    """Load statistics from the dataset."""
+    train_dir = osp.join(dataset, 'processed', 'stats', 'train')
+    mean_vec_x_train = torch.load(osp.join(train_dir, 'mean_vec_x.pt'), map_location=device)
+    std_vec_x_train = torch.load(osp.join(train_dir, 'std_vec_x.pt'), map_location=device)
+    mean_vec_edge_train = torch.load(osp.join(train_dir, 'mean_vec_edge.pt'), map_location=device)
+    std_vec_edge_train = torch.load(osp.join(train_dir, 'std_vec_edge.pt'), map_location=device)
+    mean_vec_y_train = torch.load(osp.join(train_dir, 'mean_vec_y.pt'), map_location=device)
+    std_vec_y_train = torch.load(osp.join(train_dir, 'std_vec_y.pt'), map_location=device)
+    train_stats = (mean_vec_x_train, std_vec_x_train, mean_vec_edge_train, std_vec_edge_train, mean_vec_y_train, std_vec_y_train)
+
+    val_dir = osp.join(dataset, 'processed', 'stats', 'valid')
+    mean_vec_x_val = torch.load(osp.join(val_dir, 'mean_vec_x.pt'), map_location=device)
+    std_vec_x_val = torch.load(osp.join(val_dir, 'std_vec_x.pt'), map_location=device)
+    mean_vec_edge_val = torch.load(osp.join(val_dir, 'mean_vec_edge.pt'), map_location=device)
+    std_vec_edge_val = torch.load(osp.join(val_dir, 'std_vec_edge.pt'), map_location=device)
+    mean_vec_y_val = torch.load(osp.join(val_dir, 'mean_vec_y.pt'), map_location=device)
+    std_vec_y_val = torch.load(osp.join(val_dir, 'std_vec_y.pt'), map_location=device)
+    val_stats = (mean_vec_x_val, std_vec_x_val, mean_vec_edge_val, std_vec_edge_val, mean_vec_y_val, std_vec_y_val)
+
+    test_dir = osp.join(dataset, 'processed', 'stats', 'test')
+    mean_vec_x_test = torch.load(osp.join(test_dir, 'mean_vec_x.pt'), map_location=device)
+    std_vec_x_test = torch.load(osp.join(test_dir, 'std_vec_x.pt'), map_location=device)
+    mean_vec_edge_test = torch.load(osp.join(test_dir, 'mean_vec_edge.pt'), map_location=device)
+    std_vec_edge_test = torch.load(osp.join(test_dir, 'std_vec_edge.pt'), map_location=device)
+    mean_vec_y_test = torch.load(osp.join(test_dir, 'mean_vec_y.pt'), map_location=device)
+    std_vec_y_test = torch.load(osp.join(test_dir, 'std_vec_y.pt'), map_location=device)
+    test_stats = (mean_vec_x_test, std_vec_x_test, mean_vec_edge_test, std_vec_edge_test, mean_vec_y_test, std_vec_y_test)
+
+    return train_stats, val_stats, test_stats
 
 
 def get_next_version(logs: str) -> int:
