0457697b2739a0eca7be01e162c6e5829b3bf453
UgoPelissier <upelissier@gmail.com>
2023-07-05 19:52:38 +0000

    📝 Adding name to Data for test step

--- data/dataset.py
+++ data/dataset.py
@@ -175,7 +175,7 @@ class MeshDataset(Dataset):
 
                 self.update_stats(x, edge_attr, y)
 
-                torch.save(Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y, cells=mesh.cells, mesh_pos=mesh.points, n_points=x.shape[0], n_edges=edge_index.shape[1], n_cells=mesh.cells[0].data.shape[0], name=data[:-4]),
+                torch.save(Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y, cells=torch.Tensor(mesh.cells[0].data), mesh_pos=torch.Tensor(mesh.points), n_points=x.shape[0], n_edges=edge_index.shape[1], n_cells=mesh.cells[0].data.shape[0], name=data[:-4]),
                             osp.join(self.processed_dir, self.split, f'{data[:-4]}.pt'))
                     
         self.save_stats()
