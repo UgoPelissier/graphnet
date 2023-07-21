78ed007a01f61a54986ca96d138cd58db6984177
Ugo PELISSIER <ugo.pelissier.ext@safrangroup.com>
2023-07-21 10:52:04 +0200

    🔥 Remove scripts

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wdir=/home/upelissier/30-Code/graphnet/
-ckpt_path=/data/users/upelissier/30-Code/graphnet/logs/version_0/checkpoints/epoch=900-step=169388.ckpt
-
-cd $wdir
-clear
-python main.py test --ckpt_path $ckpt_path
--- scripts/train.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wdir=/home/eleve05/safran/graphnet
-cd $wdir
-clear
-python main.py fit
